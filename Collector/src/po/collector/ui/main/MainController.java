b1faab447177b6641196753d73aa472f91d59fcc
MisterBE2 <komercyjnefy@gmail.com>
2020-06-13 14:42:03 +0200

    Poprawki + przyciski

--- Collector/src/po/collector/ui/main/MainController.java
+++ Collector/src/po/collector/ui/main/MainController.java
@@ -1,5 +1,19 @@
 package po.collector.ui.main;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class MainController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
 }

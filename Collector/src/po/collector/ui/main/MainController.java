a3940ec011b413828f7e45118a44b9062fb38ff7
KarolinaSroczyk <sigrunnthevalkyrie@gmail.com>
2020-06-14 21:05:12 +0200

    Dodano wyswietlanie obrazow

--- Collector/src/po/collector/ui/main/MainController.java
+++ Collector/src/po/collector/ui/main/MainController.java
@@ -1,22 +1,33 @@
 package po.collector.ui.main;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
+import javafx.scene.CacheHint;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import po.collector.classes.Collection;
 import po.collector.classes.Tag;
 import po.collector.classes.files.Files;
 import po.collector.classes.media.Media;
+import po.collector.classes.media.MediaView;
+
 
 public class MainController {
     private String defaultMediaURL = "C:\\PO";
@@ -50,21 +61,52 @@ public class MainController {
     private URL location;
 
     @FXML
+    private ScrollPane canvas;
+
+    @FXML
+    private AnchorPane canvas2;
+
+    @FXML
+    private Label label_test;
+
+    @FXML
     void initialize() {
         if (tab_collections.isSelected()) {
             button_add.setText("Add Collection");
         }
 
-        Runnable r = new Runnable() {
-            public void run() {
-                media.addAll(Files.loadMedia(defaultMediaURL));
-                for (Media m : media) {
-                    System.out.println(m.getResUrl());
-                }
-            }
-        };
 
-        new Thread(r).start();
+        List<HBox> hb = new ArrayList<>();
+        VBox vb = new VBox();
+        vb.setSpacing(5);
+        vb.setFillWidth(true);
+
+        try {
+            media.addAll(Files.loadMedia(defaultMediaURL));
+
+            for(int i = 0; i<media.size(); i++){
+                if(i%4 == 0){
+                    HBox tmphb = new HBox();
+                    tmphb.setSpacing(5);
+
+                    hb.add(tmphb);
+                }
+
+                MediaView im = new MediaView(media.get(i));
+                im.addEventFilter(MouseEvent.MOUSE_CLICKED, mediaViewClick);
+                im.addEventFilter(MouseEvent.MOUSE_ENTERED, mediaViewEntered);
+                im.addEventFilter(MouseEvent.MOUSE_EXITED, mediaViewLeaved);
+
+                hb.get(hb.size()-1).getChildren().add(im);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        vb.getChildren().addAll(hb);
+        canvas.setContent(vb);
+
     }
 
     @FXML
@@ -80,15 +122,11 @@ public class MainController {
     @FXML
     void tab_on_select(Event event) {
 
-        if(button_add != null) {
-            if (tab_collections.isSelected()) {
-                button_add.setText("Add Collection");
-            } else if (tab_files.isSelected()) {
-                button_add.setText("Add Source Folder");
-            } else if (tab_tags.isSelected()) {
-                button_add.setText("Add Tag");
-            }
+        if (button_add != null) {
+            Tab tab = ((Tab) event.getSource());
+            button_add.setText("Add " + tab.getText());
         }
+
     }
 
 
@@ -96,5 +134,29 @@ public class MainController {
     void text_field_search_on_key_release(KeyEvent event) {
 
     }
+
+    EventHandler<MouseEvent> mediaViewClick = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            MediaView mv = ((MediaView) e.getSource());
+            label_test.setText("Clicked: " + mv.getMedia().getResUrl());
+        }
+    };
+
+    EventHandler<MouseEvent> mediaViewEntered = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            ImageView im = ((ImageView) e.getSource());
+            im.setOpacity(0.5);
+        }
+    };
+
+    EventHandler<MouseEvent> mediaViewLeaved = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            ImageView im = ((ImageView) e.getSource());
+            im.setOpacity(1);
+        }
+    };
 
 }

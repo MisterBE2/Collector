60df65d64e686c1678e9fc05f9012853c2b7d678
MisterBE2 <komercyjnefy@gmail.com>
2020-06-15 09:12:06 +0200

    Elementy wyświetlające dane w tabach

---
+++ Collector/src/po/collector/ui/main/TabElementTypes.java
@@ -0,0 +1,7 @@
+package po.collector.ui.main;
+
+public class TabElementTypes {
+    public static final String DIRECTORY = "DIRECTORY";
+    public static final String COLLECTION = "COLLECTION";
+    public static final String TAG = "TAG";
+}
